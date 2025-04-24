4d2e3ca75a35e43cb2af224759cb598926b3fe36
Simen Sommerud <jobbsimen13@gmail.com>
2025-04-24 10:34:19 +0200

    Mappe og prosjekt laget
    mappe struktur er oppdatert sånn at appen får en egen mappe og prosjekt malen er laget.

---
+++ app/TSD2491_OBLIG1_249954/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TSD2491_OBLIG1_249954.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
